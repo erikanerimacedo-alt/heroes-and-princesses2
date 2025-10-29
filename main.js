e83642605c1503b28d7d0c22ae98a6475fdb827b
erikanerimacedo-alt <erika.neri.macedo@escola.pr.gov.br>
2025-10-29 08:01:09 -0300

    j

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ avanca.forEach(button =>{
     button.addEventListener('click', function(){
         const atual=document.querySelector('.ativo')
         const proximoPasso='passo-' + this.getAttribute('data-proximo')
+        console.log(proximoPasso)
         atual.classList.remove('ativo')
         document.getElementById(proximoPasso).classList.add('ativo')
     })
